cccf158621c17ba32f41cb8c03f318ee468d2c5e
entr0pia <30486766+entr0pia@users.noreply.github.com>
2022-01-28 23:11:11 +0800

    Update to Magisk 24

--- boot_patch.sh
+++ boot_patch.sh
@@ -70,9 +70,11 @@ fi
 # Flags
 [ -z $KEEPVERITY ] && KEEPVERITY=false
 [ -z $KEEPFORCEENCRYPT ] && KEEPFORCEENCRYPT=false
+[ -z $PATCHVBMETAFLAG ] && PATCHVBMETAFLAG=false
 [ -z $RECOVERYMODE ] && RECOVERYMODE=false
 export KEEPVERITY
 export KEEPFORCEENCRYPT
+export PATCHVBMETAFLAG
 
 chmod -R 755 .
 
@@ -98,8 +100,6 @@ case $? in
     abort "! Unable to unpack boot image"
     ;;
 esac
-
-[ -f recovery_dtbo ] && RECOVERYMODE=true
 
 ###################
 # Ramdisk Restores
@@ -135,6 +135,12 @@ case $((STATUS & 3)) in
     ;;
 esac
 
+# Work around custom legacy Sony /init -> /(s)bin/init_sony : /init.real setup
+INIT=init
+if [ $((STATUS & 4)) -ne 0 ]; then
+  INIT=init.real
+fi
+
 ##################
 # Ramdisk Patches
 ##################
@@ -143,6 +149,7 @@ ui_print "- Patching ramdisk"
 
 echo "KEEPVERITY=$KEEPVERITY" > config
 echo "KEEPFORCEENCRYPT=$KEEPFORCEENCRYPT" >> config
+echo "PATCHVBMETAFLAG=$PATCHVBMETAFLAG" >> config
 echo "RECOVERYMODE=$RECOVERYMODE" >> config
 [ ! -z $SHA1 ] && echo "SHA1=$SHA1" >> config
 
@@ -159,7 +166,7 @@ if [ -f magisk64 ]; then
 fi
 
 ./magiskboot cpio ramdisk.cpio \
-"add 0750 init magiskinit" \
+"add 0750 $INIT magiskinit" \
 "mkdir 0750 overlay.d" \
 "mkdir 0750 overlay.d/sbin" \
 "$SKIP32 add 0644 overlay.d/sbin/magisk32.xz magisk32.xz" \
